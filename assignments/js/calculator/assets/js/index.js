30ace08da507fd9cd83d9f69ce6d6399a3ce6e3f
unknown <pasanpahasara7788@gmail.com>
2022-09-26 17:52:02 +0530

    Added Javascript Code In The Calculator

--- assignments/js/calculator/assets/js/index.js
+++ assignments/js/calculator/assets/js/index.js
@@ -3,3 +3,36 @@
  * @since : 0.1.0
  **/
 
+
+let buttons = document.querySelectorAll('.number');
+let result = document.querySelector('#result');
+let clear = document.querySelector('#clearBtn');
+let history = document.querySelector('#history');
+let equalTo = document.querySelector('#equalsBtn');
+
+
+let value = "";
+
+buttons.forEach((num, index) => {
+    num.addEventListener('click', function () {
+        let text = this.innerHTML;
+        value += text;
+        result.innerHTML = value;
+    });
+});
+
+/*equal to button action*/
+equalTo.addEventListener('click', function () {
+    if (result.innerHTML !== "") {
+        history.innerHTML = result.innerHTML;
+        result.innerHTML = eval(result.innerHTML);
+        value = eval(result.innerHTML);
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please Enter Any Number!',
+        })
+    }
+});
+
