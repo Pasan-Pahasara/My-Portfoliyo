2ab8bade732f9335a187bf0938025111e9006ab4
unknown <pasanpahasara7788@gmail.com>
2022-10-04 01:14:46 +0530

    Get Text Fields Values In New Item Form In The Pos System

--- assignments/css/Frameworks/POS/assets/js/item.js
+++ assignments/css/Frameworks/POS/assets/js/item.js
@@ -2,3 +2,11 @@
  * @author : Pasan Pahasara
  * @since : 0.1.0
  **/
+
+//add new item
+$("#newItem").click(function () {
+    $("#item-id").val();
+    $("#item-name").val();
+    $("#item-price").val();
+    $("#item-quantity").val();
+});
