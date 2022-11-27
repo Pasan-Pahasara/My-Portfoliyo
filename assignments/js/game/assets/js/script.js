a545f3e04cd7fffdd715da27007501863b6aae4a
unknown <pasanpahasara7788@gmail.com>
2022-11-28 01:05:40 +0530

    Fully Refactor Idle Animation JQuery In The 2D Game

--- assignments/js/game/assets/js/script.js
+++ assignments/js/game/assets/js/script.js
@@ -2,3 +2,29 @@
  * @author : Pasan Pahasara
  * @since : 0.1.0
  **/
+
+$(document).ready(function () {
+    idleAnimationStart();
+});
+
+/***
+ * Start Idle Animation
+ * */
+let idleImageNumber = 1;
+let idleAnimationNumber = 0;
+
+function idleAnimation() {
+    idleImageNumber++;
+    if (idleImageNumber === 10) {
+        idleImageNumber = 1;
+    }
+    $("#girl").attr("src", "assets/images/png/Idle__00" + idleImageNumber + ".png");
+}
+
+function idleAnimationStart() {
+    idleAnimationNumber = setInterval(idleAnimation, 200);
+}
+
+/***
+ * End  Idle Animation
+ * */
